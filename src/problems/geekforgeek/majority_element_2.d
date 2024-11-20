1cd97d67b44aeeb5504e4b3f383f489c7c55f1bb
Animemchik <109554523+Animemchik@users.noreply.github.com>
2024-11-20 21:05:39 +0400

    Fix typo

--- src/problems/geekforgeek/majority_element_2.d
+++ src/problems/geekforgeek/majority_element_2.d
@@ -1,42 +1,42 @@
 import std;
 import std.algorithm : sort;
 /*
-     * Link: https://www.geeksforgeeks.org/problems/majority-vote/1
-     * 
-     * Majority Element II
-     * Difficulty: Medium
-     *
-     * Problem:
-     * You are given an array of integer int[] where each number represents a vote to a candidate.
-     * Return the candidates that have votes greater than one-third of the total votes, If there's not a majority vote, return an empty array. 
-     * 
-     * Note: The answer should be returned in an increasing format.
-     * 
-     * Examples:
-     * ====================================================
-     * Input: int[] array = [2, 1, 5, 5, 5, 5, 6, 6, 6, 6, 6];
-     * Output: [5, 6]
-     * Explanation: 5 and 6 occur more n/3 times.
-     * ====================================================
-     * 
-     * ====================================================
-     * Input: int[] array = [1, 2, 3, 4, 5];
-     * Output: []
-     * Explanation: no candidate occur more than n/3 times.
-     * ====================================================
-     * 
-     * Constraint:
-     * 1 <= arr.size() <= 10^6
-     * -10^9 <= arr[i] <= 10^9
-     * 
+ * Link: https://www.geeksforgeeks.org/problems/majority-vote/1
+ * 
+ * Majority Element II
+ * Difficulty: Medium
+ *
+ * Problem:
+ * You are given an array of integer int[] where each number represents a vote to a candidate.
+ * Return the candidates that have votes greater than one-third of the total votes, If there's not a majority vote, return an empty array. 
+ * 
+ * Note: The answer should be returned in an increasing format.
+ * 
+ * Examples:
+ * ====================================================
+ * Input: int[] array = [2, 1, 5, 5, 5, 5, 6, 6, 6, 6, 6];
+ * Output: [5, 6]
+ * Explanation: 5 and 6 occur more n/3 times.
+ * ====================================================
+ * 
+ * ====================================================
+ * Input: int[] array = [1, 2, 3, 4, 5];
+ * Output: []
+ * Explanation: no candidate occur more than n/3 times.
+ * ====================================================
+ * 
+ * Constraint:
+ * 1 <= arr.size() <= 10^6
+ * -10^9 <= arr[i] <= 10^9
+ * 
 */
 
-/**
-     * Returns an array of major numbers where each number represents a candidate whos amount of votes greater than one-third of the total votes.
-     *
-     * @param number the number
-     * @return the smallest (closest to negative infinity) of given
-     * {@code number}
+/*
+ * Returns an array of major numbers where each number represents a candidate whos amount of votes greater than one-third of the total votes.
+ *
+ * @param array array of votes
+ * @return an array of major numbers
+ * {@code array}
 */
 int[] find_majority(int[] array) {
     int[int] counter = new int[int];
@@ -66,7 +66,7 @@ unittest {
     assert(find_majority([1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, 1, 2, 3, 4, 5]) == []);
 }
 
-/*Program tests are performed above in the unittest block*/
+// Program tests are performed above in the unittest block
 int main(){
     return 0;
 }
